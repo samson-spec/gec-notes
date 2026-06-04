bef3a366d17277b657d48e1d8b33c42c9242a7f7
sammy <samson@g-angle.co.jp>
2026-06-04 14:15:57 +0800

    Prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,26 +1,31 @@
 import express from "express" //type: module
 // const express = require("express") //type: commonjs
+import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
-import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 
-import cors from "cors";
+
 
 dotenv.config();
 
 const app = express()
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve();
 
 // -------------------------------
 //Middleware
 
-app.use(
-    cors({
-        origin: "http://localhost:5173",
-    })
-); //enable CORS for all routes
+if(process.env.NODE_ENV !== "production"){
+    app.use(
+        cors({
+            origin: "http://localhost:5173",
+        })
+    ); //enable CORS for all routes
+}
 
 app.use(express.json()); //middleware to parse incoming JSON data: req.body
 app.use(rateLimiter); //apply rate limiting to all routes
@@ -29,6 +34,16 @@ app.use(rateLimiter); //apply rate limiting to all routes
 // -------------------------------
 
 app.use('/api/notes', notesRoutes);
+
+
+if(process.env.NODE_ENV == "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname,"../frontend", "dist","index.html"));
+    })
+}
+
 
 connectDB().then(() => {
     app.listen(PORT, () => {
